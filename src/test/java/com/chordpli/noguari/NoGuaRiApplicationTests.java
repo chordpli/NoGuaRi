73a05e549d9860568e1e5fd0290dc7fb91f05981
chordpli <chordplaylist@gmail.com>
2023-02-21 21:55:10 +0900

    build: build project

---
+++ src/test/java/com/chordpli/noguari/NoGuaRiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chordpli.noguari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoGuaRiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

69d2afdc0f1bde2b47ce37b9bea8d7f85acbf7f4
chordpli <chordplaylist@gmail.com>
2023-02-23 22:48:00 +0900

    refactor: refactor fineDustDataResponse
    1. pm10grade1h, pm25grade1h의 경우 정보를 받아올 때 null이 되는 경우가 있다는 것을 확인하여, pm10, pm25의 value 값을 통해 직접 문자로 반환할 수 있도록 로직을 구성하였음.

--- src/main/java/com/chordpli/noguari/domain/dto/FineDustDataResponse.java
+++ src/main/java/com/chordpli/noguari/domain/dto/FineDustDataResponse.java
@@ -1,5 +1,6 @@
 package com.chordpli.noguari.domain.dto;
 
+import com.chordpli.noguari.domain.dto.airpollution.AirPollutionItems;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,8 +13,59 @@ import lombok.NoArgsConstructor;
 public class FineDustDataResponse {
 
   private Object sido;
-  private Object pm10Value;
-  private Object pm25Value;
-  private Object pm10Grade1h;
-  private Object pm25Grade1h;
+  private Object stationName;
+  private Integer pm10Value;
+  private Integer pm25Value;
+  private String pm10Grade1h;
+  private String pm25Grade1h;
+
+  public static FineDustDataResponse fromEntity(AirPollutionItems airPollutionItem) {
+    int pm10Value = Integer.parseInt(airPollutionItem.getPm10Value().toString());
+    int pm25Value = Integer.parseInt(airPollutionItem.getPm25Value().toString());
+    String pm10Grade = "";
+    String pm25Grade = "";
+    pm10Grade = getPm10Grade(pm10Value);
+    pm25Grade = getPm25Grade(pm25Value);
+
+    return FineDustDataResponse.builder()
+        .sido(airPollutionItem.getSidoName())
+        .stationName(airPollutionItem.getStationName())
+        .pm10Value(pm10Value)
+        .pm10Grade1h(pm10Grade)
+        .pm25Value(pm25Value)
+        .pm25Grade1h(pm25Grade)
+        .build();
+  }
+
+  private static String getPm25Grade(int pm25Value) {
+    String pm25Grade;
+    if (0 <= pm25Value && pm25Value <= 15) {
+      pm25Grade = "좋음";
+    } else if (pm25Value <= 35) {
+      pm25Grade = "보통";
+    } else if (pm25Value <= 75) {
+      pm25Grade = "나쁨";
+    } else if (75 < pm25Value) {
+      pm25Grade = "매우나쁨";
+    } else {
+      pm25Grade = "정보없음";
+    }
+    return pm25Grade;
+  }
+
+  private static String getPm10Grade(int pm10Value) {
+    String pm10Grade;
+    if (0 <= pm10Value && pm10Value <= 30) {
+      pm10Grade = "좋음";
+    } else if (pm10Value <= 80) {
+      pm10Grade = "보통";
+    } else if (pm10Value <= 150) {
+      pm10Grade = "나쁨";
+    } else if (pm10Value > 150) {
+      pm10Grade = "매우나쁨";
+    } else {
+      pm10Grade = "정보없음";
+    }
+    return pm10Grade;
+  }
 }
